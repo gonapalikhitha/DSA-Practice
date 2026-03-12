685a33ad0abdcb019b02baa8069997d5c087dd14
gonapalikhitha <gonapalikhitha@gmail.com>
2026-03-12 06:21:08 +0530

    Sum of naturals

---
+++ Logicbuilding/FindSum.java
@@ -0,0 +1,14 @@
+public class FindSum {
+    public static int findSum(int n){
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+
+return sum;    }
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(findSum(n));
+    }
+    
+}
