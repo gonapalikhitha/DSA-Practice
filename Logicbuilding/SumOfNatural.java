7997732cdf59a40d4f2d7b9cb6c91b3d15db625c
gonapalikhitha <gonapalikhitha@gmail.com>
2026-03-12 18:12:06 +0530

    Sum of Natural numbers

---
+++ Logicbuilding/SumOfNatural.java
@@ -0,0 +1,8 @@
+public class SumOfNatural {
+    public static int sumOfNatural(int n){
+        return (n * (n + 1)) / 2;    }
+        public static void main(String[] args) {
+            System.out.println(sumOfNatural(10));
+        }
+    
+}
